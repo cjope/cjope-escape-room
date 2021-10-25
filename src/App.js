73778e17df83ae58f582d9b4da88f66b3e5bf352
Craig <craigjope@gmail.com>
2021-10-25 17:50:16 -0600

    some alerts created

--- src/App.js
+++ src/App.js
@@ -1,25 +1,69 @@
 import './App.css';
 import ReactAudioPlayer from 'react-audio-player';
-
+import "react-toastify/dist/ReactToastify.css"
+import { toast } from 'react-toastify';
+import { useState } from 'react';
 
 
 function App() {
 
+  const [candleActive, setCandleActive]=useState(false)
+  function toggleCandle(){
+    setCandleActive(true)
+  }
+  function handleLight(){
+    toggleCandle()
+  }
+  function CandleAlert(){
+    return(
+    <div>It's the black flame candle I found at an estate sale.<br/>
+        <button onClick={handleLight}>Light the Candle</button>
+        <button onClick={toast.close}>Leave it alone</button>
+      </div>
+  )}
   function handleCandleClick(e){
     e.preventDefault()
-    alert("Huh, nothing happened.")
+    toast.success(<CandleAlert />,{position: toast.POSITION.TOP_CENTER} )
   }
-
-  function handleClosedBookClick(e){
+  function handleLitCandleClick(e){
     e.preventDefault()
-    alert("Looks like it's bound in some sort of flesh.")
+    toast.success("Didn't seem to do much.")
   }
 
-  function handleOpenBookClick(e){
+
+  const [bookActive, setbookActive]=useState(false)
+  function toggleBook(){
+    setbookActive(true)
+  }
+  function handleBook(){
+    toggleBook()
+  }
+  function BookAlert(){
+    return(
+    <div >Looks like it's bound in some sort of flesh. Maybe human. Due to the face and all.<br/>
+        <button onClick={handleBook}><em>Open the book</em></button>
+        <button onClick={toast.close}>Leave it alone</button>
+      </div>
+  )}
+  function handleBookClosed(e){
     e.preventDefault()
-    alert("Klatu")
-
+    toast.success(<BookAlert/>,{position: toast.POSITION.TOP_CENTER} )
   }
+  function handleBookOpen(e){
+    e.preventDefault()
+    toast.error(<BookOpenAlert/>,{position: toast.POSITION.TOP_CENTER})
+  }
+
+  function BookOpenAlert(){
+    return(
+    <div><em>Necronomicon Ex-Mortis...<br/></em><b>The book of the Dead</b></div>
+  )}
+
+
+
+
+
+
 
   function handleClockClick(e){
     e.preventDefault()
@@ -51,46 +95,26 @@ function App() {
     alert("This junk came right off! If I could get out of this room, I'd give the landlord a piece of my mind!")
   }
 
+  toast.configure()
+
 
   window.onload = function() {
     var backgroundAudio=document.getElementById("audio");
-    backgroundAudio.volume=.07;
-    // backgroundAudio.play()
+    backgroundAudio.volume = .1
+
 }
 
 
 
 
   return (
-    // <div className="App">
-      /* <div className="flex">
-        <div className="one">1</div>
-        <div className="two">2</div>
-        <div className="three">3</div>
-        <div className="four">4</div>
-        <div className="five">5</div>
-        <div className="six">6</div>
-        <div className="seven">7</div>
-        <div className="eight">8</div>
-        <div className="nine">9</div>
-      </div> */
-      // <a className="candle"></a>
-
-      /* <iframe src="https://giphy.com/embed/mZNSjf7LSnxTEEdzcZ" width="480" height="185" frameBorder="0" class="giphy-embed" allowFullScreen></iframe> */
-      /* <img src={process.env.PUBLIC_URL + '/candle_unlit.gif'} alt="candle_unlit" className="candle" /> */
-      /* <a className="candle" ></a> */
-      /* <img href="./candle_unlit.jpeg" alt="candle"></img> */
-    // </div>
-    
-    // <div className="darkness">
-
-
-
-
       <div className="background-pic">
-        <a className="candle" href="candle-unlit1"onClick={handleCandleClick}> </a>
-        <a className="necronomicon-closed" href="necronomicon1" onClick={handleClosedBookClick}> </a>
-        <a className="necronomicon-open" href="necronomicon2" onClick={handleOpenBookClick}> </a>
+        {!candleActive?<a className="candle"href="candle-unlit1"onClick={handleCandleClick}> </a>:
+        <a className="candle-lit"href="candle-lit"onClick={handleLitCandleClick}> </a>}
+
+        {!bookActive?<a className="necronomicon-closed" href="necronomicon1" onClick={handleBookClosed}> </a>:
+        <a className="necronomicon-open" href="necronomicon2" onClick={handleBookOpen}> </a>}
+
         <a className="clock" href="clock"onClick={handleClockClick}> </a>
         <a className="fish" href="fish"onClick={handleFishClick}> </a>
         <a className="dresser-drawer" href="dresser-drawer"onClick={handleDresserDrawerClick}> </a>
@@ -98,26 +122,12 @@ function App() {
         <a className="redrum-wallpaper" href="redrum-wallpaper"onClick={handleRedrumClick}> </a>
         <a className="mirror" href="mirror"onClick={handleMirrorClick}> </a>
         {/* <a className="mirror-redrum" href="mirror-redrum"onClick={handleCandleClick}> </a> */}
-{/*         
-        <audio id="audio" type="audio/mpeg" src="../public/the_8_bit_charleston.mp3" allow="autoplay"> </audio>*/}
+        {/* <a className="candle-lit" href="candle"></a> */}
         
-        {/* <iframe src="https://github.com/anars/blank-audio/blob/master/250-milliseconds-of-silence.mp3" title="silence" allow="autoPlay" id="audio" style={{display:"none"}}></iframe> */}
-
-        {/* <iframe src="../public/the_8_bit_charleston.mp3" autoPlay loop style={{display:"none"}}  id="audio" title="charleston" ></iframe>  */}
-        <ReactAudioPlayer
-          src="the_8_bit_charleston.mp3"
-          id="audio"
-          autoPlay
-          controls
-          loop
-          volume="2"        />
-        {/* <audio autoPlay loop  id="playAudio"> */}
-          {/* <source src="../public/the_8_bit_charleston.mp3"/> */}
-        {/* </audio> */}
+        <ReactAudioPlayer src="the_8_bit_charleston.mp3" id="audio" autoPlay controls loop/>
 
         {/* <div className="darkness"> </div>  when necronomicon is opened, loop this to make the screen flash*/}
       </div>
-    // </div>
 
 
 
